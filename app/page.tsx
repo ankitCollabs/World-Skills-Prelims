f8613dd7a4147ad4f7d7d5342aa4c4f8ce225dab
ankitCollabs <ankitb.collab@gmail.com>
2026-01-06 01:47:34 -0800

    tasks implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
+import {redirect} from "next/navigation"
+
 export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen items-center justify-center">
-      <h1 className="text-xl font-bold">Hello World Test Ankit</h1>
-    </div>
-  );
+
+  redirect("/09_module_a")
 }
